e93cc4077702a79e55ac438c7396c14d30da6a90
Gabriel Carlos <105656968+cssgabriel@users.noreply.github.com>
2022-06-03 15:59:31 -0300

    Add files via upload

---
+++ Modulo 01/ex02.c
@@ -0,0 +1,43 @@
+// #include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    int height;
+    char space = " ";
+    int decrement;
+    
+    do
+    {
+        height = get_int("Height: ");
+    } while (height < 1 || height > 8);
+
+    decrement = height - 1;
+    
+    for (int i = 1; i <= height; i++)
+    {
+        // imprime os espaços
+        for (int j = decrement; j > 0; j--)
+        {
+        printf("%s", space);
+        }
+        
+        // imprime os blocos
+        for (int x = decrement; x < height; x++)
+        {
+            printf("#");
+        }
+        
+        printf("%s", space);
+        printf("%s", space);
+        
+        //imprime os blocos do segundo lado
+        for (int x = decrement; x < height; x++)
+        {
+            printf("#");
+        }
+        
+    printf("\n");
+    decrement--;
+    }
+}
